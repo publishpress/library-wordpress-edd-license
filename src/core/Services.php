6dcdce40c5bc384aaa1f356e07fbcdd6f49f952f
Anderson Grudtner Martins <anderson@ostraining.com>
2018-05-18 14:02:00 -0300

    Bump patch version

--- src/core/Services.php
+++ src/core/Services.php
@@ -83,7 +83,7 @@ class Services implements \Pimple\ServiceProviderInterface
          */
         $pimple['LIBRARY_VERSION'] = function (Container $c)
         {
-            return '2.2.0';
+            return '2.2.1';
         };
 
         $pimple['API_URL'] = function (Container $c)
