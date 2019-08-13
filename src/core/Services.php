3a60b859aa302f96c49468b6d4a1aac6d26c9c05
Anderson Grudtner Martins <anderson@alledia.com>
2019-08-13 16:37:14 -0300

    Update the version number

--- src/core/Services.php
+++ src/core/Services.php
@@ -83,7 +83,7 @@ class Services implements \Pimple\ServiceProviderInterface
          */
         $pimple['LIBRARY_VERSION'] = function (Container $c)
         {
-            return '2.2.5';
+            return '2.2.6';
         };
 
         $pimple['API_URL'] = function (Container $c)
