cf2644e4751df38ffcd3615683a5420b29cef087
Anderson Grudtner Martins <anderson@publishpress.com>
2020-04-20 19:13:59 -0300

    Add test for the core/License class

---
+++ tests/wpunit/Core/LicenseCest.php
@@ -0,0 +1,54 @@
+<?php namespace Core;
+
+use PublishPress\EDD_License\Core\License;
+use WpunitTester;
+
+class LicenseCest
+{
+    const DUMMY_PLUGIN_VERSION = '0.1.0';
+
+    public function testValidate_license_keyReturnsInvalidForInvalidLicenseKey(WpunitTester $I)
+    {
+        $itemId = '234';
+
+        $container = $I->getEddContainer(self::DUMMY_PLUGIN_VERSION, $itemId, '', '');
+
+        $licenseHandler = new License($container);
+
+        $result = $licenseHandler->validate_license_key('000000000000000', $itemId);
+
+        $I->assertIsString($result);
+        $I->assertEquals('invalid', $result);
+    }
+
+    public function testValidate_license_keyReturnsValidForValidLicenseKey(WpunitTester $I)
+    {
+        $validItemId     = $_ENV['PUBLISHPRESS_VALID_ITEM_ID'];
+        $validLicenseKey = $_ENV['PUBLISHPRESS_VALID_LICENSE_KEY'];
+
+        $container = $I->getEddContainer(self::DUMMY_PLUGIN_VERSION, $validItemId, '', '');
+
+        $licenseHandler = new License($container);
+
+        $result = $licenseHandler->validate_license_key($validLicenseKey, $validItemId);
+
+        $I->assertIsString($result);
+        $I->assertEquals('valid', $result);
+    }
+
+    public function testValidate_license_keyDoesntThrowsExceptionForNotResolvableApiUrl(WpunitTester $I)
+    {
+        $validItemId         = $_ENV['PUBLISHPRESS_VALID_ITEM_ID'];
+        $validLicenseKey     = $_ENV['PUBLISHPRESS_VALID_LICENSE_KEY'];
+        $notResolvableApiUrl = 'http://invalid.site.com';
+
+        $container = $I->getEddContainer(self::DUMMY_PLUGIN_VERSION, $validItemId, '', '', $notResolvableApiUrl);
+
+        $licenseHandler = new License($container);
+
+        $result = $licenseHandler->validate_license_key($validLicenseKey, $validItemId);
+
+        $I->assertIsString($result);
+        $I->assertStringContainsString('Sorry, an error occurred', $result);
+    }
+}
