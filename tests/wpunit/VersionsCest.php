acea8bbbb37c0ab485acfe07d97a6b5e2951fb84
Anderson Martins <anderson@publishpress.com>
2023-05-30 16:12:58 -0300

    Bump version to 3.0.1

--- tests/wpunit/VersionsCest.php
+++ tests/wpunit/VersionsCest.php
@@ -21,7 +21,7 @@ class VersionsCest
         $I->assertEquals([
             '2.0.0.1' => 'PublishPress\WordpressEddLicense\initialize2Dot0Dot0Dot1',
             '2.0.0.2' => 'PublishPress\WordpressEddLicense\initialize2Dot0Dot0Dot2',
-            '3.0.0' => 'PublishPress\WordpressEddLicense\initialize3Dot0Dot0',
+            '3.0.1' => 'PublishPress\WordpressEddLicense\initialize3Dot0Dot1',
         ], $registeredVersions);
     }
 
@@ -31,7 +31,7 @@ class VersionsCest
 
         $latestVersion = $versions->latestVersion();
 
-        $I->assertEquals('3.0.0', $latestVersion);
+        $I->assertEquals('3.0.1', $latestVersion);
     }
 
     public function testLatestVersionCallbackIsTheLastOne(WpunitTester $I)
@@ -40,7 +40,7 @@ class VersionsCest
 
         $latestVersionCallback = $versions->latestVersionCallback();
 
-        $I->assertEquals('PublishPress\WordpressEddLicense\initialize3Dot0Dot0', $latestVersionCallback);
+        $I->assertEquals('PublishPress\WordpressEddLicense\initialize3Dot0Dot1', $latestVersionCallback);
     }
 
     public function testInitializeLatestVersion(WpunitTester $I)
@@ -51,7 +51,7 @@ class VersionsCest
 
         $I->assertTrue(class_exists('PublishPress\WordPressEDDLicense\License'));
 
-        $didAction = (bool)did_action('publishpress_wordpress_edd_license_3Dot0Dot0_initialized');
+        $didAction = (bool)did_action('publishpress_wordpress_edd_license_3Dot0Dot1_initialized');
         $I->assertTrue($didAction);
     }
 }
